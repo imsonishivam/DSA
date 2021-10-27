b453a89dbd3675732537d5fe4802d83d3553ef01
imsonishivam <imsonishivam@gmail.com>
2021-10-28 04:56:25 +0530

    leftmost char which not repeat

---
+++ string/leftNoteRepeating.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int nonRepeating(string str)
+{
+    int count[256];
+    int res = INT_MAX;
+    memset(count,-1,sizeof(count));
+    for(int i=0;i<str.length();i++)
+    {
+        if(count[str[i]] == -1)
+            count[str[i]] = i;
+        else{
+            count[str[i]] = -2;
+        }
+    }
+    for(int i=0;i<256;i++)
+    {
+        if(count[i]>=0)
+        {
+            res = min(res,count[i]);
+        }
+    }
+    return res;
+}
+int main()
+{
+    string str;
+    cin>>str;
+    cout<<nonRepeating(str);
+}
