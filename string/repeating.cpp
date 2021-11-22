d8689f98764c7de49152569715e112acdf1c24f4
imsonishivam <imsonishivam@gmail.com>
2021-11-22 08:08:33 +0530

    IMPROVEMENT

--- string/repeating.cpp
+++ string/repeating.cpp
@@ -1,22 +1,27 @@
 #include<iostream>
 using namespace std;
+
+char repeating(string str){
+    int count[256],res = INT_MAX;
+    memset(count,-1,sizeof(count));
+    for(int i=0;i<str.length();i++){
+        if(count[str[i]] == -1)
+            count[str[i]] = i;
+        else
+            res = min(res,count[str[i]]);
+    }
+    // (res == INT_MAX) ? return '#' : return str[res]; 
+    if(res == INT_MAX)
+        return '#';
+    else
+        return  str[res];
+}
+
 int main()
 {
     int res = INT_MAX;
     string str;
     cin>>str;
-    int count[256];
-    memset(count,-1,sizeof(count));
-    for(int i=0;i<str.length();i++)
-    {
-        if(count[str[i]] == -1)
-            count[str[i]] = i;
-        else{
-            res = min(res,count[str[i]]);
-        }
-    }
-    if(res == INT_MAX)
-    cout<<"-1";
-    else
-    cout<<res;
+    char repeating_char = repeating(str);
+    repeating_char == '#' ? cout<<"-1" : cout<<repeating_char;
 }
